a52482100e4f1508c1b5fd1365ab998ec5d2be87
Chad Fennell <fenne035@umn.edu>
2016-02-23 09:13:56 -0600

    ensures file open gets to args

--- test/loaders/file_loader_test.rb
+++ test/loaders/file_loader_test.rb
@@ -4,7 +4,7 @@ require 'digest'
 
 class FileLoaderTest < Minitest::Test
   extend Minitest::Spec::DSL
-  let(:mock_file) {MiniTest::Mock.new.expect :open, nil, [String]}
+  let(:mock_file) {MiniTest::Mock.new.expect :open, nil, [String, String]}
 
   def test_load_file
     Letl::FileLoader.new(args: {dir: 'foo'}, state: {},file_class: mock_file).load!
